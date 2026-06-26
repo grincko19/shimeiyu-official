09acfc591b80052d6f4b86e0d545c320f3b6dceb
grincko19 <grincko19@gmail.com>
2026-06-27 05:50:34 +0800

    Create LogoIcon.tsx

---
+++ components/logo/LogoIcon.tsx
@@ -0,0 +1,30 @@
+export default function LogoIcon(){
+
+return(
+
+<svg
+width="42"
+height="42"
+viewBox="0 0 42 42"
+fill="none"
+xmlns="http://www.w3.org/2000/svg"
+>
+
+<path
+d="M21 4
+C16 12
+17 18
+22 24
+C27 30
+27 35
+21 38"
+stroke="#1E352B"
+strokeWidth="2.2"
+strokeLinecap="round"
+/>
+
+</svg>
+
+)
+
+}
