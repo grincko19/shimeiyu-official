656130429597d742f71fc96f774b0ca3e5cc518c
grincko19 <grincko19@gmail.com>
2026-06-27 01:05:26 +0800

    Create Hero.tsx

---
+++ components/home/Hero.tsx
@@ -0,0 +1,31 @@
+import Button from "../ui/Button";
+
+export default function Hero(){
+
+return(
+
+<section className="h-screen flex flex-col justify-center items-center">
+
+<h1 className="text-6xl font-serif">
+
+施美玉名香祖舖
+
+</h1>
+
+<p className="mt-6 text-xl">
+
+Since 1774
+
+</p>
+
+<Button>
+
+探索商品
+
+</Button>
+
+</section>
+
+)
+
+}
