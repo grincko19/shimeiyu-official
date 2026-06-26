019f69e0a8589660cd19ea4178c0173cdde558ed
grincko19 <grincko19@gmail.com>
2026-06-27 05:45:22 +0800

    Create Logo.tsx

---
+++ components/logo/Logo.tsx
@@ -0,0 +1,31 @@
+import LogoIcon from "./LogoIcon";
+
+export default function Logo(){
+
+return(
+
+<div className="flex items-center gap-3">
+
+<LogoIcon/>
+
+<div>
+
+<h3>
+
+SHI MEI YU
+
+</h3>
+
+<p>
+
+Since 1774
+
+</p>
+
+</div>
+
+</div>
+
+)
+
+}
