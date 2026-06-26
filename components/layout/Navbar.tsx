3e1c77061b55a048083ef5e9e59f53f3a6945d9f
grincko19 <grincko19@gmail.com>
2026-06-27 01:05:49 +0800

    Add Navbar component with navigation links

---
+++ components/layout/Navbar.tsx
@@ -0,0 +1,33 @@
+export default function Navbar(){
+
+return(
+
+<header className="fixed top-0 w-full bg-white/80 backdrop-blur-lg">
+
+<nav className="max-w-7xl mx-auto h-20 flex justify-between items-center">
+
+<h2>
+
+SHI MEI YU
+
+</h2>
+
+<div className="flex gap-10">
+
+<a href="/">首頁</a>
+
+<a href="/about">品牌故事</a>
+
+<a href="/products">商品</a>
+
+<a href="/contact">聯絡我們</a>
+
+</div>
+
+</nav>
+
+</header>
+
+)
+
+}
