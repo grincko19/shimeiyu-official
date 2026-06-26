17b10f39e0b7babbbe084a5d783e27acd1b3159b
grincko19 <grincko19@gmail.com>
2026-06-27 01:06:04 +0800

    Create Footer.tsx

---
+++ components/layout/Footer.tsx
@@ -0,0 +1,21 @@
+export default function Footer(){
+
+return(
+
+<footer className="border-t py-10 mt-24">
+
+<div className="max-w-7xl mx-auto">
+
+<p>
+
+©2026 SHI MEI YU
+
+</p>
+
+</div>
+
+</footer>
+
+)
+
+}
