5ae3edf445da750cd01e01d09b89db60fbfeba5d
grincko19 <grincko19@gmail.com>
2026-06-27 18:49:12 +0800

    Create StorySection.tsx

---
+++ components/home/StorySection.tsx
@@ -0,0 +1,43 @@
+import Section from "../ui/Section";
+
+import Heading from "../ui/Heading";
+
+import Button from "../ui/Button";
+
+export default function StorySection(){
+
+return(
+
+<Section>
+
+<Heading
+
+title="品牌故事"
+
+subtitle="Since1774"
+
+/>
+
+<p>
+
+施美玉名香祖舖創立於清乾隆年間，
+
+至今已有超過250年的製香經驗，
+
+堅持天然原料，
+
+延續台灣傳統香藝。
+
+</p>
+
+<Button>
+
+閱讀更多
+
+</Button>
+
+</Section>
+
+)
+
+}
