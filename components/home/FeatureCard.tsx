b842ff8d46417b870f9052242cc50f94f2000b0a
grincko19 <grincko19@gmail.com>
2026-06-27 19:09:58 +0800

    Update FeatureCard.tsx

--- components/home/FeatureCard.tsx
+++ components/home/FeatureCard.tsx
@@ -1,39 +1,20 @@
 import Card from "../ui/Card";
 
-interface Props{
-
-title:string;
-
-text:string;
-
+interface Props {
+  title: string;
+  text: string;
 }
 
-export default function FeatureCard({
+export default function FeatureCard({ title, text }: Props) {
+  return (
+    <div className="group rounded-3xl border p-10 bg-white transition duration-500 hover:-translate-y-2 hover:shadow-xl">
+      <h3 className="text-xl font-semibold group-hover:text-[#1E352B] transition">
+        {title}
+      </h3>
 
-title,
-
-text
-
-}:Props){
-
-return(
-
-<Card>
-
-<h3>
-
-{title}
-
-</h3>
-
-<p className="mt-5">
-
-{text}
-
-</p>
-
-</Card>
-
-)
-
+      <p className="mt-4 text-gray-600 leading-7">
+        {text}
+      </p>
+    </div>
+  );
 }
