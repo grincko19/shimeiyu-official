5e6f1bb4f6732e25c8c0d7d41d84296ddd5b6c45
grincko19 <grincko19@gmail.com>
2026-06-27 18:49:50 +0800

    Create page.tsx

---
+++ components/contact/page.tsx
@@ -0,0 +1,23 @@
+export default function ContactPage(){
+
+return(
+
+<div className="max-w-5xl mx-auto py-32">
+
+<h1>
+
+聯絡我們
+
+</h1>
+
+<p className="mt-8">
+
+歡迎透過電話或Email與我們聯繫。
+
+</p>
+
+</div>
+
+)
+
+}
