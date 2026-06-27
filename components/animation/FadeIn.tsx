3fe1c835b0653654fdddf773d050e9ff6b8202ce
grincko19 <grincko19@gmail.com>
2026-06-27 19:04:43 +0800

    Create FadeIn.tsx

---
+++ components/animation/FadeIn.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { ReactNode } from "react";
+
+export default function FadeIn({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.8 }}
+    >
+      {children}
+    </motion.div>
+  );
+}
