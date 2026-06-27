071d0d1ba09e48e8f81e1abd08f9c1012e53e0e7
grincko19 <grincko19@gmail.com>
2026-06-27 19:14:54 +0800

    Create sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,22 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://example.com",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://example.com/products",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://example.com/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://example.com/contact",
+      lastModified: new Date(),
+    },
+  ];
+}
