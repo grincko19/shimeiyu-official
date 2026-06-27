3718e3af2e53ac8b3ceab2aee85a8317893a9ce2
grincko19 <grincko19@gmail.com>
2026-06-27 18:49:34 +0800

    Create page.tsx

---
+++ components/products/page.tsx
@@ -0,0 +1,23 @@
+export default function ProductsPage(){
+
+return(
+
+<div className="max-w-7xl mx-auto py-32">
+
+<h1>
+
+商品總覽
+
+</h1>
+
+<p className="mt-8">
+
+商品將依分類呈現。
+
+</p>
+
+</div>
+
+)
+
+}
