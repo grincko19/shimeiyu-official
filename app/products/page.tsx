fec5df97a70dfb71b4cae115692586beba464170
grincko19 <grincko19@gmail.com>
2026-06-27 19:02:39 +0800

    Update page.tsx

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,24 +1,17 @@
-import CategoryMenu from "@/components/products/CategoryMenu";
-import ProductGrid from "@/components/products/ProductGrid";
+import ProductList from "@/components/products/ProductList";
 
-export default function ProductsPage(){
+export default function ProductsPage() {
+  return (
+    <div className="max-w-7xl mx-auto py-32 px-8">
+      <h1 className="text-5xl font-bold mb-4">
+        商品總覽
+      </h1>
 
-return(
+      <p className="text-gray-500 mb-10">
+        依照商品分類瀏覽施美玉名香祖舖商品。
+      </p>
 
-<div className="max-w-7xl mx-auto py-32 px-8">
-
-<h1 className="text-5xl mb-10">
-
-商品總覽
-
-</h1>
-
-<CategoryMenu/>
-
-<ProductGrid/>
-
-</div>
-
-)
-
+      <ProductList />
+    </div>
+  );
 }
