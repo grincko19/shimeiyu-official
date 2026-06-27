ae18c29f59d79a3444fd51148cce44b13cf0ec4f
grincko19 <grincko19@gmail.com>
2026-06-27 18:48:41 +0800

    Create page.tsx

---
+++ components/home/page.tsx
@@ -0,0 +1,27 @@
+import Hero from "@/components/home/Hero";
+
+import Feature from "@/components/home/Feature";
+
+import ProductSection from "@/components/home/ProductSection";
+
+import StorySection from "@/components/home/StorySection";
+
+export default function HomePage(){
+
+return(
+
+<>
+
+<Hero/>
+
+<Feature/>
+
+<ProductSection/>
+
+<StorySection/>
+
+</>
+
+)
+
+}
