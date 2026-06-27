7e56eba1339ebd212a54c55953e0e9b5ffb13dc5
grincko19 <grincko19@gmail.com>
2026-06-27 19:05:30 +0800

    Refactor Hero component to use HeroBackground and HeroContent

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,31 +1,11 @@
-import Button from "../ui/Button";
+import HeroBackground from "./HeroBackground";
+import HeroContent from "./HeroContent";
 
-export default function Hero(){
-
-return(
-
-<section className="h-screen flex flex-col justify-center items-center">
-
-<h1 className="text-6xl font-serif">
-
-施美玉名香祖舖
-
-</h1>
-
-<p className="mt-6 text-xl">
-
-Since 1774
-
-</p>
-
-<Button>
-
-探索商品
-
-</Button>
-
-</section>
-
-)
-
+export default function Hero() {
+  return (
+    <section className="h-screen flex items-center justify-center relative overflow-hidden">
+      <HeroBackground />
+      <HeroContent />
+    </section>
+  );
 }
