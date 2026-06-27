435fd17a1672203b1823a305131a730102533ed8
grincko19 <grincko19@gmail.com>
2026-06-27 19:15:53 +0800

    Create page.tsx

---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+export default function AboutPage() {
+  return (
+    <div className="max-w-5xl mx-auto py-32 px-8">
+      <h1 className="text-5xl">品牌故事</h1>
+
+      <p className="mt-10 leading-8 text-gray-700">
+        施美玉名香祖舖創立於清乾隆年間，
+        至今已有超過250年的歷史。
+        堅持天然原料與手工製香技藝，
+        延續台灣傳統香文化。
+      </p>
+    </div>
+  );
+}
